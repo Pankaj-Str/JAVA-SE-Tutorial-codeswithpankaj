d900103f949283f3782c55c226813380c3dd650c
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-06-30 16:40:16 +0530

    Create Data_type.java

---
+++ Day_1/Data_type.java
@@ -0,0 +1,54 @@
+//@p4n.in
+public class Data_type {
+
+	public static void main(String[] args) {
+		
+		byte bytemin,bytemax;
+		bytemin = Byte.MIN_VALUE;
+		bytemax = Byte.MAX_VALUE;
+		System.out.println("Byte MIN Value : "+bytemin);
+		System.out.println("Byte MAX Value : "+bytemax);
+		
+		short shortmin,shortmax;
+		shortmin = Short.MIN_VALUE;
+		shortmax = Short.MAX_VALUE;
+		System.out.println("short MIN Value : "+shortmin);
+		System.out.println("short MAX Value : "+shortmax);
+		
+		int intmin,intmax;
+		intmin = Integer.MIN_VALUE;
+		intmax = Integer.MAX_VALUE;
+		System.out.println("int MIN Value : "+intmin);
+		System.out.println("int MAX Value : "+intmax);
+		
+		long longmin,longmax;
+		longmin = Long.MIN_VALUE;
+		longmax = Long.MAX_VALUE;
+		System.out.println("long MIN Value : "+longmin);
+		System.out.println("long MAX Value : "+longmax);
+		
+		float floatmin,floatmax;
+		floatmin = Float.MIN_VALUE;
+		floatmax = Float.MAX_VALUE;
+		System.out.println("Float MIN Value : "+floatmin);
+		System.out.println("Float MAX Value : "+floatmax);
+		
+		double doublemin,doublemax;
+		doublemin = Double.MIN_VALUE;
+		doublemax = Double.MAX_VALUE;
+		System.out.println("Double MIN Value : "+doublemin);
+		System.out.println("Double MAX Value : "+doublemax);
+			
+		char ch = 'A';
+		
+		System.out.println(ch);
+		
+		String name = "Joy";
+		
+		System.out.println(name);
+		
+	
+
+	}
+
+}
