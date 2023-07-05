f72445866940034daa2c7a4c8a4cd080d04d63bc
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-07-05 16:13:19 +0530

    Add files via upload

---
+++ Day_1/userinput.java
@@ -0,0 +1,32 @@
+package Day02;
+import java.util.Scanner;
+public class userinput {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name,address;
+        int age;
+        float height;
+        char div;
+        System.out.println("Enter Your address : ");
+        address = sc.nextLine();
+        System.out.println("Enter Your name : ");
+        name = sc.next();
+        System.out.println("Enter Your age : ");
+        age = sc.nextInt();
+        System.out.println("Enter Your height : ");
+        height = sc.nextFloat();
+
+        System.out.println("Enter Your Div : ");
+        div = sc.next().charAt(0);
+        System.out.println("---------------------");
+        System.out.println("My name is : = "+name);
+        System.out.println("My age is : = "+age);
+        System.out.println("My height is : = "+height);
+        System.out.println("My address is : = "+address);
+        System.out.println("My div is : = "+div);
+
+
+
+    }
+}
