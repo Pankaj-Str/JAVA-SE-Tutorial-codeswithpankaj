5470379cf44359b5004ebb8096724cd7a5917691
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-07-07 16:41:20 +0530

    Add files via upload

---
+++ Day_2/RelationalOperators.java
@@ -0,0 +1,18 @@
+package Day03;
+
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+
+        System.out.println("Relational Operators  < :"+(45 < 89));
+        System.out.println("Relational Operators  > :"+(45 > 89));
+        System.out.println("Relational Operators  <= :"+(45 <= 89));
+        System.out.println("Relational Operators  >= :"+(45 >= 89));
+        System.out.println("Relational Operators  == :"+(45 == 89));
+        System.out.println("Relational Operators  != :"+(45 != 89));
+
+
+
+
+    }
+}
