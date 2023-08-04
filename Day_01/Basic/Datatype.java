3c03b62db6fe9f5c15879bc075c00ada58aea869
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-08-04 13:42:35 +0000

    update datatype..

---
+++ Day_01/Basic/Datatype.java
@@ -0,0 +1,30 @@
+package Basic;
+
+public class Datatype {
+
+    public static void main(String[] args) {
+        //https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+        
+        byte b = 'L';
+
+        System.out.println("\n byte = \n"+b);
+
+        char ltr = 45;
+
+        System.out.println("\n char = \n"+ltr);
+
+        // byte 
+
+        byte MinByte,MaxByte;
+
+        MinByte = Byte.MIN_VALUE;
+        MaxByte = Byte.MAX_VALUE;
+
+        System.out.println("Byte Min Value :- "+MinByte);
+        System.out.println("Byte Max Value :- "+MaxByte);
+
+        
+
+    }
+    
+}
---
+++ Day_01/Basic/Variable.java
@@ -0,0 +1,16 @@
+package Basic;
+
+class Variable{
+
+    public static void main(String[] args) {
+        
+        //code ....
+
+        //DataType VariableName = Value;
+
+        int Number = 23;
+
+
+    }
+
+}
