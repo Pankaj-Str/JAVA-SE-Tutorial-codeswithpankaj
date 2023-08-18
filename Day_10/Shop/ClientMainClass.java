002e4ece48853aa2559d7c8bae322ce3df8747f7
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-08-18 08:46:32 +0530

    Add files via upload

---
+++ Day_10/Shop/ClientMainClass.java
@@ -0,0 +1,24 @@
+package Shop;
+import Date_16_08_2023.School;
+
+import  java.util.Scanner;
+public class ClientMainClass {
+    public static void main(String[] args) {
+        // create a object
+        Scanner sc = new Scanner(System.in);
+        // ClassName ObjectName = new ClassName();
+        Client cl = new Client();
+        System.out.println("Enter Client Name : ");
+        String name = sc.next();
+        System.out.println("Enter Client Mobile number : ");
+        long mobile = sc.nextLong();
+
+        cl.name = name;
+        cl.mobile = mobile;
+        cl.info();
+
+//        cl.name = "Toy";
+//        cl.mobile = 7897541230l;
+//        cl.info();
+    }
+}
