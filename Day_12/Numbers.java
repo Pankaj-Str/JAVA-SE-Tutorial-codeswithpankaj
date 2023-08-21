594d9b20b77f481ae05e242d69301523e969ed12
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-08-21 16:44:06 +0530

    Add files via upload

---
+++ Day_12/Numbers.java
@@ -0,0 +1,16 @@
+package Aug_21_2023;
+
+public class Numbers
+{
+
+    Numbers(int a , int b){
+        int c = a+b;
+        System.out.println(c);
+    }
+    public static void main(String[] args) {
+        Numbers obj = new Numbers(45,96);
+        Numbers obj1 = new Numbers(450,996);
+    }
+
+
+}
