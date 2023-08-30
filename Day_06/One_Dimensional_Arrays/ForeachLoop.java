f3dc0aeb6ac80e2fe8ebd96aea6310125d733743
Pankaj Chouhan <{pankaj-str}@users.noreply.github.com>
2023-08-30 18:42:44 +0530

    Update One Dimensional Arrays

---
+++ Day_06/One_Dimensional_Arrays/ForeachLoop.java
@@ -0,0 +1,16 @@
+package One_Dimensional_Arrays;
+
+public class ForeachLoop {
+    
+    public static void main(String[] args) {
+        String students[] = {"Vinit","Deepak","Rohit","Joy"};
+        
+        for (String std : students) {
+            
+            System.out.println(std);
+        }
+    }
+    
+    
+
+}
