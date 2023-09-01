0ae161235a252ddf9ca57d69086ac4c4c8dac282
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-09-01 10:32:40 +0000

    update Day 13

---
+++ Day_13/Encapsulation1/Client.java
@@ -0,0 +1,14 @@
+class Clinet{
+
+    String ClinetName;
+    long Mobile;
+
+    void SetClinetName(String ClinetName){
+        this.ClinetName = ClinetName;
+    }
+
+    void SetMobile(int Mobile){
+        this.Mobile = Mobile;
+    }
+
+} 
