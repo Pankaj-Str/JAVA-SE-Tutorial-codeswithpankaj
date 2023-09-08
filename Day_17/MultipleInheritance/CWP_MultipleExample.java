4a8fb9bda4d3c9751efb5f3c55ed641c0a411379
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-09-08 10:29:22 +0000

    update ... Muliple

---
+++ Day_17/MultipleInheritance/CWP_MultipleExample.java
@@ -0,0 +1,38 @@
+package Day_17.MultipleInheritance;
+
+
+class student{
+        void GetStudentInfo(){
+            System.out.println("Student : Mumtaz");
+            System.out.println("Student  : Joy");
+        }
+}
+class books extends student{
+        void GetBooks(){
+            System.out.println("JAVA Development");
+            System.out.println("Python development");
+        }
+}
+class Subject extends student{
+        void GetSubejct(){
+            System.out.println("Inheritance....");
+            System.out.println("Numpy....");
+        } 
+}
+
+
+public class CWP_MultipleExample {
+        public static void main(String[] args) {
+          
+          
+          books book = new books();
+          book.GetBooks();
+          book.GetStudentInfo();  
+
+          Subject subjects = new Subject();
+          subjects.GetSubejct();
+          subjects.GetStudentInfo();
+
+
+        }
+}
