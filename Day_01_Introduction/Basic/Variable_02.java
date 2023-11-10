298ebdc36c3429bb7577c2483fd83213bd667825
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-11-10 10:09:31 +0530

    Create Variable_02.java

---
+++ Day_01_Introduction/Basic/Variable_02.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class Variable {
+	
+	public static void main(String[] arg) {
+		
+		// create a variable
+		int age;
+		String name;
+		float height;
+		// assign values
+		
+		age = 23;
+		name = "Sahil";
+		height = 5.9f;
+		
+		System.out.println("Your Name is : "+name);
+		System.out.println("Your Age is : "+age);
+		System.out.println("Your Height is : "+height);
+		
+		
+	}
+
+}
