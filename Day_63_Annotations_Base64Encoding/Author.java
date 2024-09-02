d7c072b31a5f3a73d5422022e6090379a53873e3
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-09-02 06:30:42 +0000

    update Annotations and Base64 Encoding in Java

---
+++ Day_63_Annotations_Base64Encoding/Author.java
@@ -0,0 +1,17 @@
+package Day_63_Annotations_Base64Encoding;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)  // Annotation is available at runtime
+
+@Target({ElementType.TYPE, ElementType.METHOD})  // Can be used on classes and methods
+
+public @interface Author {
+
+    String name();
+
+    String date();
+
+}
---
+++ Day_63_Annotations_Base64Encoding/MyClass.java
@@ -0,0 +1,15 @@
+package Day_63_Annotations_Base64Encoding;
+
+@Author(name = "Pankaj", date = "2024-09-02")
+
+public class MyClass {
+
+    @Author(name = "Pankaj", date = "2024-09-02")
+
+    public void myMethod() {
+
+        System.out.println("This method is annotated!");
+
+    }
+
+}
