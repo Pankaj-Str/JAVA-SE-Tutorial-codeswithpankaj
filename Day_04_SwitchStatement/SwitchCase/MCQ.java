e03a26422f9a22adf7835fc1d371b9a183cea517
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-12-18 03:04:26 +0000

    update JAVA MCQ Switch case File...

---
+++ Day_04_SwitchStatement/SwitchCase/MCQ.java
@@ -0,0 +1,14 @@
+public class MCQ {
+
+    public static void main(String[] agr){
+
+        int a=90;
+        int b=80;
+
+        int c = a + b;
+        System.out.println(c);
+
+    }
+    
+
+}
