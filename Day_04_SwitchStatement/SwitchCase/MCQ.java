b68e97bef46b65a0496a427eed6d1441ad5e57b3
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-12-18 03:12:12 +0000

    update MCQ ... java Question file

--- Day_04_SwitchStatement/SwitchCase/MCQ.java
+++ Day_04_SwitchStatement/SwitchCase/MCQ.java
@@ -1,12 +1,37 @@
+import java.util.Scanner;
+
 public class MCQ {
 
     public static void main(String[] agr){
 
-        int a=90;
-        int b=80;
+        // object of Scanner class
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Who invented Java Programming?\r\n" + //
+                        "a) Guido van Rossum\r\n" + //
+                        "b) James Gosling\r\n" + //
+                        "c) Dennis Ritchie\r\n" + //
+                        "d) Bjarne Stroustrup\n");
 
-        int c = a + b;
-        System.out.println(c);
+        char option = sc.next().charAt(0);
+        
+        switch (option) {
+            case 'a': System.out.println("Wrong Answer");
+            break;
+            case 'b': System.out.println("Answer: b \r\n" + //
+                                "Explanation: Java programming was developed by James Gosling at Sun Microsystems in 1995. James Gosling is well known as the father of Java.");
+            break;
+            case 'c': System.out.println("Wrong Answer");
+            break;
+            case 'd': System.out.println("Wrong Answer");
+            break;
+            default :
+            System.out.println("select only a,b,c,d");
+            
+        }
+
+        
+
+        
 
     }
     
