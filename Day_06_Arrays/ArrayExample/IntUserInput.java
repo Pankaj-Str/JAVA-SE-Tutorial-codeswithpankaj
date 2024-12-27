4971e528436c6a1d7f705ef861d8145c5a50d86d
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-12-27 03:08:27 +0000

    add int input into array

---
+++ Day_06_Arrays/ArrayExample/IntUserInput.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class IntUserInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter a size of array 
+
+        int size;
+
+        System.out.println("Enter Array size .... ");
+        size = scanner.nextInt();
+
+        int [] array = new int[size];
+
+
+        
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter Array elements .... ["+i+"] ");
+            array[i] = scanner.nextInt();
+        }
+    }
+
+
+}
