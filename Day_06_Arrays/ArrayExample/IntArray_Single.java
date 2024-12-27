02c230118a097ec6eb4ab19f048cdfac48166b32
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-12-27 03:04:45 +0000

    add example of array

---
+++ Day_06_Arrays/ArrayExample/IntArray_Single.java
@@ -0,0 +1,36 @@
+public class IntArray_Single {
+
+
+    public static void main(String[] args) {
+        
+
+           // array of integers
+
+           int data[] = new int[4];
+
+           // assigning values to array
+
+           data[0] = 21;
+           data[1] = 45;
+           data[2] = 67;
+           data[3] = 89;
+
+           // printing single item from array
+
+           // System.out.println("this is element of array : "+data[2]);
+
+           // print all item from array 
+
+           for (int i = 0; i < data.length; i++) {
+            
+            System.out.println("Item from array : "+data[i]);
+
+           }
+
+
+
+
+
+    }
+    
+}
